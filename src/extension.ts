e67fa24480047f5918081e4440c7c9e73acd015a
minaaaayk <mina.yaghubi95@gmail.com>
2022-06-21 13:31:28 +0430

    :construction: add question command

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "arvanTest" is now active!');
+	
+	context.subscriptions.push(
+		vscode.commands.registerCommand('arvanTest.helloWorld', () => {
+			vscode.window.showInformationMessage('this is test of arvan extention');
+		}
+	));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('arvanTest.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('this is test of arvan extention');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('arvanTest.arvanQuestion',async () => {
+			const answer = await vscode.window.showInformationMessage('how are you?', 'good', 'bad');
+			console.log('answer: ', answer);
+		}
+	));
 }
 
 // this method is called when your extension is deactivated
